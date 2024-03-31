e0fde1719902ed7e724ce51f20451a380dbb0428
Gavin <gavin.wade12@gmail.com>
2024-03-31 18:38:22 -0400

    feat: sort the DTCs

--- cmd/logger-ui/tab_dtcs.go
+++ cmd/logger-ui/tab_dtcs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"sort"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -18,7 +19,8 @@ const (
 type DTCsTab struct {
 	app *App
 
-	grid *fyne.Container
+	grid       *fyne.Container
+	refreshBtn *widget.Button
 }
 
 func NewDTCsTab(app *App) *DTCsTab {
@@ -26,17 +28,20 @@ func NewDTCsTab(app *App) *DTCsTab {
 		app:  app,
 		grid: container.NewGridWithColumns(2),
 	}
+	tab.refreshBtn = widget.NewButton("Refresh", tab.refresh)
 	return tab
 }
 
 func (t *DTCsTab) Container() fyne.CanvasObject {
 	return container.NewVScroll(container.NewVBox(
-		widget.NewButton("Refresh", t.refresh),
+		t.refreshBtn,
 		t.grid,
 	))
 }
 
 func (t *DTCsTab) refresh() {
+	t.refreshBtn.Disable()
+	defer t.refreshBtn.Enable()
 	t.grid.RemoveAll()
 
 	if t.app.connection == nil {
@@ -51,12 +56,14 @@ func (t *DTCsTab) refresh() {
 		logger.Debug(err.Error())
 		return
 	}
+	sort.Sort(sortableDTCs(setDTCs))
 
 	storedDTCs, err := t.readDTCs(true)
 	if err != nil {
 		logger.Debug(err.Error())
 		return
 	}
+	sort.Sort(sortableDTCs(storedDTCs))
 
 	for _, dtc := range setDTCs {
 		t.grid.Objects = append(t.grid.Objects,
@@ -83,3 +90,9 @@ func (t *DTCsTab) readDTCs(stored bool) ([]ssm2.DTC, error) {
 	}
 	return ssm2.ReadSetDTCs(ctx, t.app.connection)
 }
+
+type sortableDTCs []ssm2.DTC
+
+func (a sortableDTCs) Len() int           { return len(a) }
+func (a sortableDTCs) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a sortableDTCs) Less(i, j int) bool { return a[i].Name < a[j].Name }
